5fc7e0eee895d234adb828bdb8c331ce25bd7ee9
dongmy54 <615760433@qq.com>
2018-09-08 13:58:39 +0800

    update

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,12 +15,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*args)
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  
+    #{}"/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    'timg.jpg'
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -29,18 +30,28 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  process resize_to_fit: [500, 500]
+  process resize_to_fit: [60, 60]
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [200, 200]
+  version :large do
+    process resize_to_fit: [500, 500]
   end
+
+  version :middle do
+    process resize_to_fit: [120, 120]
+  end
+
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
